74e8343b27dfab97a83892aec8ac57097773e3f9
MohamedHalim <mohamedhalim1998@gmail.com>
2021-11-23 16:03:50 +0200

    ADD: basic routes to add and get card

--- Server/app.py
+++ Server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, json, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from database_setup import CardSet, db
+from database_setup import CardSet, Card, db
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
@@ -31,6 +31,32 @@ def add_card_set():
         db.session.add(cardSet)
         db.session.commit()
         return jsonify(cardSet.serialize())
+
+
+@app.route('/card')
+def get_card():
+    data = Card.query.all()
+    data = list(map(Card.serialize, data))
+    return jsonify(data)
+
+
+@app.route('/card/add', methods=["POST"])
+def add_card():
+    if (request.method == 'POST'):
+        data = json.loads(request.args.get("data"))
+        print(data)
+        card = Card(
+            setId=data["setId"],
+            front=data["front"],
+            back=data["back"],
+        )
+        db.session.add(card)
+        cardSet = CardSet.query.filter_by(id=data['setId']).all()
+        print(cardSet[0].count)
+        cardSet[0].count += 1
+        # db.session.add(cardSet)
+        db.session.commit()
+        return jsonify(card.serialize())
 
 
 if __name__ == '__main__':
