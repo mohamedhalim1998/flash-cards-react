b95fadee3ac70a37e5ad51a5221ac9fe05349139
MohamedHalim <mohamedhalim1998@gmail.com>
2021-11-26 20:53:20 +0200

    FIX: center content using margin auto

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ function Home() {
     dispatch(loadCardSets());
   }, []);
   return (
-    <div className="container grid md:grid-cols-3 gap-4 pt-8 mx-8">
+    <div className="container grid md:grid-cols-3 gap-4 pt-8 mx-auto px-8">
       {cardsets.map((cardset) => (
         <CardSetCard
           key={cardset.id}
