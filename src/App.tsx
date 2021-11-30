dc6be061e0c1c04b6ff387483ced42b276065a79
MohamedHalim <mohamedhalim1998@gmail.com>
2021-11-30 13:59:01 +0200

    ADD: route to cards page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import NewSet from "./pages/NewSet";
+import Cards from "./pages/Cards";
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/cardset/new" element={<NewSet />} />
-        <Route path="/cardset" element={<Home />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+          <Route path="/cardset/:id" element={<Cards />} />
+          <Route path="/cardset" element={<Home />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
     </div>
   );
 }
--- src/components/CardSet.tsx
+++ src/components/CardSet.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import CardSet from "../data/CardSet";
 
-
-
 function CardSetCard(cardset: CardSet) {
+  const navigate = useNavigate();
   return (
-    <div className="card cursor-pointer">
-      <h2 className="pt-8 px-4 font-bold text-xl  text-left">{cardset.name}</h2>
+    <div
+      className="card cursor-pointer"
+      onClick={(e) => {
+        navigate(`/cardset/${cardset.id}`);
+      }}
+    >
+      <h2 className="px-4 font-bold text-xl  text-left">{cardset.name}</h2>
       <h3 className="pb-4 px-4 text-xl text-left text-gray-500">{`${cardset.description}`}</h3>
       <h3 className="px-4 text-xl text-left text-gray-500">{`${cardset.count} terms`}</h3>
     </div>
